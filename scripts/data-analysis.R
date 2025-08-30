ad31903443ce03eaf17d59ef665ce913c2be4f9b
mu-05 <murad.blue53@gmail.com>
2025-08-30 17:38:22 +0200

    Update the dataset, script and report
    - Updated dataset to include full data
    - Added data processing steps in the R script
    - Began drafting and coding sections in the
    report

--- scripts/data-analysis.R
+++ scripts/data-analysis.R
@@ -1,5 +1,7 @@
 # Load libraries
 library(tidyverse)
+library(DBI)
+library(RSQLite)
 
 # Get working directory
 getwd()
@@ -10,4 +12,31 @@ sales <- read_csv(file = "data/sales.csv")
 # Glimpse
 glimpse(sales)
 
+# Convert date column from POSIXct to character 
+sales$date <- as.Date(x = sales$date)
+sales$date <- format(sales$date, "%Y-%m-%d")
+class(sales$date)
 
+# Create SQLite connection
+con <- dbConnect(
+  drv = RSQLite::SQLite(), 
+  dbname = ":memory:")
+
+# Write the data frame to the database
+dbWriteTable(
+  conn = con,
+  name = "sales",
+  value = sales
+)
+
+# See the data using R code
+DBI::dbGetQuery(con, "SELECT * FROM sales LIMIT 10;")
+
+# See column details
+DBI::dbGetQuery(con, "PRAGMA table_info(sales)")
+
+# Get month number using strftime() function
+DBI::dbGetQuery(con, "SELECT strftime('%m', date) FROM sales")
+
+# Close the connection
+dbDisconnect(con)
